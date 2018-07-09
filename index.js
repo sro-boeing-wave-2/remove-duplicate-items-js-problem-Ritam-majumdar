e4720d9035c25bb7d98451ab1ceb0e5bf2a34fd4
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-07-09 15:57:14 +0530

    npm installed and modified(vol_2) index.js

--- index.js
+++ index.js
@@ -3,33 +3,26 @@
  * @param {Array} inpArray
  * @returns {Array}
  */
-const removeDuplicateItems = (inpArray) {
-inpArray = new Array;
-outArray = new Array;
-var i= 0;
-var j= 0;
-var k=0;
-var x;
-for(i=0;i<inpArray.length;i++)
-{
-    x=0;
-    for(j=0;j<outArray.length;j++)
-    {
-        if (outArray[j]==inpArray[i])
-        {
-            x=1;
-            break;
-        }
+const removeDuplicateItems = (inpArray) => {
+  const outArray = [];
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  let x;
+  for (i = 0; i < inpArray.length; i += 1) {
+    x = 0;
+    for (j = 0; j < outArray.length; j += 1) {
+      if (outArray[j] === inpArray[i]) {
+        x = 1;
+        break;
+      }
     }
-        if(x==0)
-        {
-        outArray[k]=inpArray[i];
-        k++;
-        }
-        else
-        continue;
-}
-return outArray;
+    if (x === 0) {
+      outArray[k] = inpArray[i];
+      k += 1;
+    }
+  }
+  return outArray;
 };
 
 module.exports = removeDuplicateItems;
